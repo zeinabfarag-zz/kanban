296012688b506300da82eed3aabe70b13149f571
Zeinab Farag <zeinabfarag@Zeinabs-MacBook-Pro.local>
2020-03-25 10:43:00 -0400

    commit

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const createError = require("http-errors");
 const express = require("express");
 const { join } = require("path");
 const cookieParser = require("cookie-parser");
@@ -27,22 +26,6 @@ app.use("/user", userRouter);
 app.use("/dashboards", dashboardRouter);
 app.use("/calendar", calendarRouter);
 app.use("/file", fileRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.json({ error: err });
-});
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 
