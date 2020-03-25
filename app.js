02eb660ae6339aa048ea88c63ba8b17ae2b7e4b6
Zeinab Farag <zeinabfarag@Zeinabs-MacBook-Pro.local>
2020-03-25 09:42:03 -0400

    path names

--- app.js
+++ app.js
@@ -44,10 +44,10 @@ app.use(function(err, req, res, next) {
   res.json({ error: err });
 });
 
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(PORT, function() {
