e2554de0ce2395dba6e6e79223d033aeaeeec48f
Zeinab Farag <zeinabfarag@Zeinabs-MacBook-Pro.local>
2020-03-25 10:35:50 -0400

    commit

--- app.js
+++ app.js
@@ -44,13 +44,11 @@ app.use(function(err, req, res, next) {
   res.json({ error: err });
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, function() {
   console.log(`Listening on ${PORT}`);
